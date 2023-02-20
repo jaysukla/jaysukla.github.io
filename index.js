3a08816f1ad0cdfd733f36e603d3fd64f5da79ee
jaysukla <am7343249@gmail.com>
2023-02-20 21:33:31 +0530

    new update

---
+++ index.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector("#resume-button-1")
+
+btn.addEventListener("click",redirec)
+
+
+function redirec(){
+
+location.href="https://drive.google.com/file/d/14cpb-yIeYybLQTfq981lfUEBUu_N8i17/view?usp=share_link"
+
+}
+
