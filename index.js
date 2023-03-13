b1462f5d14409e2be31e7bfdbc12185e0d4f8c8b
jaysukla <am7343249@gmail.com>
2023-03-13 19:47:46 +0530

    calender

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// resume button 
 let btn = document.querySelector("#resume-button-1")
 
 btn.addEventListener("click",redirec)
@@ -5,7 +7,14 @@ btn.addEventListener("click",redirec)
 
 function redirec(){
 
-location.href="https://drive.google.com/file/d/14cpb-yIeYybLQTfq981lfUEBUu_N8i17/view?usp=share_link"
+window.open("https://drive.google.com/file/d/14cpb-yIeYybLQTfq981lfUEBUu_N8i17/view?usp=share_link","blank")
 
 }
+
+// resume button 
+
+
+GitHubCalendar(".calendar", "jaysukla", { responsive: true });
+
+
 
