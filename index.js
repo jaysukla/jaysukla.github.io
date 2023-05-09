3cb0aff422b63f7fce7c12f7701d396c19683c73
jaysukla <am7343249@gmail.com>
2023-05-09 17:24:15 +0530

    complete change

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
-
-// resume button 
-// let btn = document.querySelector("#resume-button-1")
-// let btn2=document.querySelector("#resume-button-2")
-// btn2.addEventListener("click",redirec)
-// btn.addEventListener("click",redirec)
-
-
-function redirec(){
-
-window.open("https://drive.google.com/file/d/14cpb-yIeYybLQTfq981lfUEBUu_N8i17/view?usp=share_link","blank")
-
-}
-
-// resume button 
-
-
-GitHubCalendar(".calendar", "jaysukla", { responsive: true });
-
-
-
-
-
-
-
-
+function resume(){
+    window.open("https://drive.google.com/file/d/17IAPamudP8UhGqDUfI3wpcyEFqwex7IU/view?usp=sharing","_blank")
+  }
+  
+  
+  $(document).ready(function(){
+  
+      $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+      });
+    
+      $(window).on('scroll load',function(){
+    
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+    
+        if($(window).scrollTop() > 0){
+          $('.top').show();
+        }else{
+          $('.top').hide();
+        }
+    
+      });
+    
+      // smooth scrolling 
+    
+      $('a[href*="#"]').on('click',function(e){
+    
+        e.preventDefault();
+    
+        $('html, body').animate({
+    
+          scrollTop : $($(this).attr('href')).offset().top,
+    
+        },
+          500, 
+          'linear'
+        );
+    
+      });
+    
+    });
