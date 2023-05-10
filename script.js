c58ddff67c27b4efc54ee9db80de5a1149432af8
jaysukla <am7343249@gmail.com>
2023-05-10 11:18:18 +0530

    click

--- script.js
+++ script.js
@@ -3,43 +3,43 @@ function resume(){
 }
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    $('#menu').click(function(){
-      $(this).toggleClass('fa-times');
-      $('header').toggleClass('toggle');
-    });
+//     $('#menu').click(function(){
+//       $(this).toggleClass('fa-times');
+//       $('header').toggleClass('toggle');
+//     });
   
-    $(window).on('scroll load',function(){
+//     $(window).on('scroll load',function(){
   
-      $('#menu').removeClass('fa-times');
-      $('header').removeClass('toggle');
+//       $('#menu').removeClass('fa-times');
+//       $('header').removeClass('toggle');
   
-      if($(window).scrollTop() > 0){
-        $('.top').show();
-      }else{
-        $('.top').hide();
-      }
+//       if($(window).scrollTop() > 0){
+//         $('.top').show();
+//       }else{
+//         $('.top').hide();
+//       }
   
-    });
+//     });
   
-    // smooth scrolling 
+//     // smooth scrolling 
   
-    $('a[href*="#"]').on('click',function(e){
+//     $('a[href*="#"]').on('click',function(e){
   
-      e.preventDefault();
+//       e.preventDefault();
   
-      $('html, body').animate({
+//       $('html, body').animate({
   
-        scrollTop : $($(this).attr('href')).offset().top,
+//         scrollTop : $($(this).attr('href')).offset().top,
   
-      },
-        500, 
-        'linear'
-      );
+//       },
+//         500, 
+//         'linear'
+//       );
   
-    });
+//     });
   
-  });
+//   });
 
   GitHubCalendar(".calendar", "jaysukla", { responsive: true });
